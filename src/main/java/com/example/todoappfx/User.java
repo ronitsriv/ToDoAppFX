ff8fd0f32f2fa22d39836cd0693fbc48a449a2a9
Ronit Srivastava <ronitsriv@gmail.com>
2023-11-06 15:32:42 +0530

    Added getters, setters and constructors in the User class.

--- src/main/java/com/example/todoappfx/User.java
+++ src/main/java/com/example/todoappfx/User.java
@@ -4,4 +4,25 @@ public class User {
     private String loginID;
 
     private String password;
+
+    public User(String loginID, String password) {
+        this.loginID = loginID;
+        this.password = password;
+    }
+
+    public String getLoginID() {
+        return loginID;
+    }
+
+    public void setLoginID(String loginID) {
+        this.loginID = loginID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
