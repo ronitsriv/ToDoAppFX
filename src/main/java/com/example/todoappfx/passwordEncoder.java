7897d2247f894687c5342da5c11b166fc043d70b
Ronit Srivastava <ronitsriv@gmail.com>
2023-11-06 15:31:04 +0530

    The encoding seems pretty good. I included a print function to see what it is. I don't know what it does but the final password doesn't even have same number of characters as the initial one.

--- src/main/java/com/example/todoappfx/passwordEncoder.java
+++ src/main/java/com/example/todoappfx/passwordEncoder.java
@@ -3,10 +3,14 @@ package com.example.todoappfx;
 import java.util.Base64;
 
 public class passwordEncoder {
-    private String encodingLogic(String password){
+    private static String encodingLogic(String password){
+        //System.out.println(Base64.getEncoder().encodeToString(password.getBytes()));
         return Base64.getEncoder().encodeToString(password.getBytes());
     }
 
+    public static void main(String[] args) {
+        encodingLogic("helloworld");
+    }
     public String encodePassword(String input){
         return encodingLogic(input);
     }
